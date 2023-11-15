381d2aaf8ab9a2fa38b4f3ed85724de35058da69
Sveta <kutsvet100com@gmail.com>
2023-11-15 15:54:02 +0200

    Addded fragment to a diary screen

--- app/src/main/java/com/example/project/Diary_screen.java
+++ app/src/main/java/com/example/project/Diary_screen.java
@@ -1,29 +1,41 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.project.databinding.ActivityMainScreenBinding;
+import com.google.android.material.bottomappbar.BottomAppBarTopEdgeTreatment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Diary_screen extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_screen);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new Fragment_diary()).commit();
+        bottomNavigationView=findViewById(R.id.bottom_navigation);
         bottom_navigation();
+
     }
     private void bottom_navigation(){
-        BottomNavigationView bottomNavigationView1=findViewById(R.id.bottom_navigation);
-        bottomNavigationView1.setSelectedItemId(R.id.navigation_diary);
-        bottomNavigationView1.setOnItemSelectedListener(item -> {
+
+        bottomNavigationView.setSelectedItemId(R.id.navigation_diary);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
             if(item.getItemId()==R.id.navigation_home){
                 startActivity(new Intent(Diary_screen.this, Main_screen.class));
                 finish();
                 return true;}
             else if (item.getItemId()==R.id.navigation_diary){
+
                 return true;
             }
             else if (item.getItemId()==R.id.navigation_graphs){
@@ -38,5 +50,12 @@ public class Diary_screen extends AppCompatActivity {
             }
             return false;
         });
+    }
+    private void replace_fragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
     }
 }
